5caaa17819492310b0063e30e1ec080f46e3da58
yangbo05527 <yangbo@suneee.com>
2018-07-30 10:27:41 +0800

    无关调整

--- src/main/java/com/pattern/proxy/ProxyTest.java
+++ src/main/java/com/pattern/proxy/ProxyTest.java
@@ -20,7 +20,7 @@ public class ProxyTest {
         // 实例化InvocationHandler
         MyInvocationHandler invocationHandler = new MyInvocationHandler(userService);
 
-        // 根据目标对象生成代理对象(代理对象强制转换目标对象)
+        // 根据目标对象生成代理对象(强制转换Object成代理对象)
         UserService proxy = (UserService) invocationHandler.getProxy();
 
         // 调用代理对象($Proxy11)的方法
