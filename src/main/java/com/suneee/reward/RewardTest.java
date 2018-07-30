f245c4baa263264772f3c652ead76fd6c9796d8f
yangbo05527 <yangbo@suneee.com>
2018-07-30 11:06:13 +0800

    dev rewardtest文件 合并到master

--- src/main/java/com/suneee/reward/RewardTest.java
+++ src/main/java/com/suneee/reward/RewardTest.java
@@ -11,10 +11,9 @@
 package com.suneee.reward;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class RewardTest {
     public static void main(String[] args) {
@@ -22,6 +21,7 @@ public class RewardTest {
     }
 
     public static void ruanTimeTest(){
+
         long start = System.currentTimeMillis();
         List<RewardItem> rewardItemList=new ArrayList<RewardItem>();
         RewardItem rewardItem1=new RewardItem();
