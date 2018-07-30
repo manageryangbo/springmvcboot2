d2f7e5b269f20c408a6e2a6bf431adffffc09a0a
yangbo05527 <yangbo@suneee.com>
2018-07-30 14:54:28 +0800

    分支 第一版本打印记录

--- src/main/java/com/pattern/single/SingleTest.java
+++ src/main/java/com/pattern/single/SingleTest.java
@@ -16,6 +16,6 @@ import java.util.stream.Collectors;
 public class SingleTest {
     public static void main(String[] args) {
 //        EnumSingleton.uniqueInstance.singletonOperation();  // 枚举的单例模式
-
+        System.out.println("分支1 打印");
     }
 }
