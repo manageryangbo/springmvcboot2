8152e6bac284442b6736ed79bee894e2cd415f61
yangbo05527 <yangbo@suneee.com>
2018-07-30 10:23:08 +0800

    无关功能调整

---
+++ src/main/java/com/suneee/validates/SimpleHappenBefore.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: SimpleHappenBefore
+ * Author:   martin
+ * Date:     2018/7/23 16:57
+ * Description: 指令重排序
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名          修改时间          版本号            描述
+ */
+package com.suneee.validates;
+
+public class SimpleHappenBefore {
+}
