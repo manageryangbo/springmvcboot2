619330196e5f893e7c0d6c16d23b183587980894
yangbo05527 <yangbo@suneee.com>
2018-11-02 09:29:31 +0800

    批量改包

---
+++ src/main/java/com/jdk8/com/suneee/dome/Apple.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Apple
+ * Author:   martin
+ * Date:     2018/11/2 9:20
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名          修改时间          版本号            描述
+ */
+package com.jdk8.com.suneee.dome;
+
+public class Apple {
+    private Integer id ;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/jdk8/lambda/Test.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Test
+ * Author:   martin
+ * Date:     2018/11/2 9:21
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名          修改时间          版本号            描述
+ */
+package com.jdk8.lambda;
+
+import com.jdk8.com.suneee.dome.Apple;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Apple ap=  new Apple();
+    }
+
+}
