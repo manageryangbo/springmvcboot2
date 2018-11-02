2a3e5d0ef006fcda440cb6f35a7d78a2fc4be49f
yangbo05527 <yangbo@suneee.com>
2018-11-02 09:41:48 +0800

    批量改包

--- src/main/java/com/jdk8/com/suneee/dome2/Apple.java
+++ src/main/java/com/jdk8/com/suneee/dome2/Apple.java
@@ -8,7 +8,7 @@
  * <author>          <time>          <version>          <desc>
  * 作者姓名          修改时间          版本号            描述
  */
-package com.jdk8.com.suneee.dome;
+package com.jdk8.com.suneee.dome2;
 
 public class Apple {
     private Integer id ;
