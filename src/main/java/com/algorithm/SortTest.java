d00fe2bbb376260d033dd20ca7ce531d91124fee
yangbo05527 <yangbo@suneee.com>
2019-07-16 16:50:55 +0800

    排序算法

---
+++ src/main/java/com/algorithm/SortTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: SortTest
+ * Author:   martin
+ * Date:     2019/7/15 14:53
+ * Description: 排序测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名          修改时间          版本号            描述
+ */
+package com.algorithm;
+
+import java.util.Arrays;
+
+public class SortTest {
+
+    public static void main(String[] args) {
+
+        int[] ints1 = new int[]{2,9,6,1,5,8,4,3};
+        SortUtil.bubbleSortAsc(ints1);
+        System.out.println( "1-最终结果:" + Arrays.toString(ints1));
+        System.out.println( "循环次数:"+SortUtil.totalCount + "交换次数:"+SortUtil.changeCount );
+
+        System.out.println("===============================================");
+
+        int[] ints2 = new int[]{2,9,6,1,5,8,4,3};
+        SortUtil.bubbleSortDesc( ints2 );
+        System.out.println( "2-最终结果:" + Arrays.toString(ints2));
+        System.out.println( "循环次数:"+SortUtil.totalCount + "交换次数:"+SortUtil.changeCount );
+
+        System.out.println("===============================================");
+
+        int[] ints3 = new int[]{2,9,6,1,5,8,4,3};
+        SortUtil.selectorSortAsc( ints3 );
+        System.out.println( "2-最终结果:" + Arrays.toString(ints3));
+        System.out.println( "循环次数:"+SortUtil.totalCount + "交换次数:"+SortUtil.changeCount );
+
+
+    }
+}
---
+++ src/main/java/com/algorithm/SortUtil.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: SortUtil
+ * Author:   martin
+ * Date:     2019/7/15 14:53
+ * Description:
+ *          排序算法帮助类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名          修改时间          版本号            描述
+ */
+package com.algorithm;
+
+
+import java.util.Arrays;
+
+public class SortUtil {
+
+    public static int totalCount = 0 ; // 计数器
+    public static int changeCount = 0 ; // 交换计数
+
+    /**
+     * 冒泡排序算法[顺势]
+     * @param array
+     */
+     public static void bubbleSortAsc(int[] array){
+           for(int i=0 ; i< array.length ; i++){
+               for (int j=i+1;j<array.length;j++){
+                   if(array[i]>array[j]){
+                       int temp = array[i];
+                       array[i] = array[j];
+                       array[j] = temp ;
+                       changeCount++;
+                   }
+                   totalCount ++ ;
+               }
+               System.out.println(  Arrays.toString( array ) ); // 顺势打印效果
+           }
+     }
+
+    /**
+     * 冒泡排序算法[逆势]
+     * @param array
+     */
+    public static void bubbleSortDesc(int[] array){
+        int length = array.length;
+        for(int i=0 ; i< length ; length -- ){
+            int index = i;
+            for (int j=index+1;j<length;j++,index++){
+                if(array[index]>array[j]){
+                    int temp = array[index];
+                    array[index] = array[j];
+                    array[j] = temp ;
+                    changeCount++;
+                }
+                totalCount ++ ;
+            }
+            System.out.println(  Arrays.toString( array ) );  // 逆势打印效果
+        }
+    }
+
+    /**
+     * 选择排序算法[顺势]
+     * @param array
+     */
+    public static void selectorSortAsc(int[] array){
+        int length = array.length;
+        for(int i=0 ; i< length ; i++){
+            int minIndex = i;
+            for (int j=i+1;j<length;j++){
+                minIndex = array[j]<array[minIndex]?j:minIndex;
+                totalCount ++ ;
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp ;
+            changeCount ++ ;
+            System.out.println(  Arrays.toString( array ) );  // 逆势打印效果
+        }
+    }
+
+}
